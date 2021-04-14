f958a253d991d12232446780208e16e0f8fe28ac
Aleksandr Matrosov <aleksandr.matrosov@7-tv.de>
2021-04-14 12:08:20 +0200

    Initial Commit

---
+++ connect/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  connect
+//
+//  Created by Aleksandr Matrosov on 14.04.21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ connect/connectApp.swift
@@ -0,0 +1,17 @@
+//
+//  connectApp.swift
+//  connect
+//
+//  Created by Aleksandr Matrosov on 14.04.21.
+//
+
+import SwiftUI
+
+@main
+struct connectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
